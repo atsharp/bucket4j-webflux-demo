d8412eefc26f370376a4c8ba3e0fafb66b818362
Aaron Sharp <aaron.sharp@clover.com>
2021-05-12 20:26:50 -0700

    simpler

--- src/test/java/com/bucket4j/demo/DemoApplicationTests.java
+++ src/test/java/com/bucket4j/demo/DemoApplicationTests.java
@@ -1,65 +1,48 @@
 package com.bucket4j.demo;
 
-import com.giffing.bucket4j.spring.boot.starter.config.Bucket4JBaseConfiguration;
 import com.giffing.bucket4j.spring.boot.starter.config.cache.AsyncCacheResolver;
-import com.giffing.bucket4j.spring.boot.starter.config.webflux.Bucket4JAutoConfigurationWebfluxFilter;
-import com.giffing.bucket4j.spring.boot.starter.context.ConsumptionProbeHolder;
-import com.giffing.bucket4j.spring.boot.starter.context.RateLimitCheck;
-import com.giffing.bucket4j.spring.boot.starter.context.RateLimitConditionMatchingStrategy;
-import com.giffing.bucket4j.spring.boot.starter.context.metrics.MetricHandler;
-import com.giffing.bucket4j.spring.boot.starter.context.properties.Bucket4JBootProperties;
-import com.giffing.bucket4j.spring.boot.starter.context.properties.Bucket4JConfiguration;
-import com.giffing.bucket4j.spring.boot.starter.context.properties.FilterConfiguration;
 import com.giffing.bucket4j.spring.boot.starter.filter.reactive.webflux.WebfluxWebFilter;
+import io.github.bucket4j.AsyncBucket;
 import io.github.bucket4j.Bucket;
 import io.github.bucket4j.BucketConfiguration;
+import io.github.bucket4j.ConsumptionProbe;
 import io.github.bucket4j.grid.ProxyManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilterChain;
+import org.springframework.web.server.WebFilter;
 import reactor.core.publisher.Mono;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.Collection;
 import java.util.Optional;
-import java.util.function.Supplier;
+import java.util.concurrent.CompletableFuture;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
 
-	@MockBean
-	AsyncCacheResolver asyncCacheResolver;
 	@Autowired
 	private WebTestClient webTestClient;
+	@Autowired
+	private Collection<WebFilter> webFilters;
 
 	@Test
 	void testRateLimiting() {
-		ProxyManager<String> proxyManager = mock(ProxyManager.class);
-		doReturn(proxyManager).when(asyncCacheResolver).resolve(any());
-		final Bucket bucket = mock(Bucket.class);
-		doReturn(Optional.of(bucket)).when(proxyManager).getProxy(any());
-		doReturn(bucket).when(proxyManager).getProxy(any(), any(Supplier.class));
-		doReturn(bucket).when(proxyManager).getProxy(any(), any(BucketConfiguration.class));
-		doReturn(0L).when(bucket).getAvailableTokens();
-		// there are 0 tokens, so this should be rate-limited
+		assertThat(webFilters)
+				.anyMatch(filter -> filter instanceof WebfluxWebFilter);
 		webTestClient.get()
 				.uri("/test-rate-limit")
 				.exchange()
@@ -69,12 +52,29 @@ class DemoApplicationTests {
 
 	@Configuration
 	@EnableAutoConfiguration
-	@EnableConfigurationProperties(Bucket4JBootProperties.class)
 	static class Config  {
+
+		private WebFilter delegate;
 
 		@Bean
 		public AsyncCacheResolver asyncCacheResolver() {
-			return mock(AsyncCacheResolver.class);
+			final AsyncCacheResolver cacheResolver = mock(AsyncCacheResolver.class);
+			doReturn(proxyManager()).when(cacheResolver).resolve(any());
+			return cacheResolver;
+		}
+
+		@Bean
+		public ProxyManager<String> proxyManager() {
+			final Bucket bucket = mock(Bucket.class);
+			final AsyncBucket asyncBucket = mock(AsyncBucket.class);
+			final ConsumptionProbe consumptionProbe = ConsumptionProbe.rejected(0, 1000);
+			doReturn(CompletableFuture.completedFuture(consumptionProbe)).when(asyncBucket).tryConsumeAndReturnRemaining(anyLong());
+			doReturn(asyncBucket).when(bucket).asAsync();
+			final ProxyManager<String> proxyManager = mock(ProxyManager.class);
+			doReturn(Optional.of(bucket)).when(proxyManager).getProxy(any());
+			doReturn(bucket).when(bucket).toListenable(any());
+			doReturn(bucket).when(proxyManager).getProxy(any(), any(BucketConfiguration.class));
+			return proxyManager;
 		}
 
 		@RestController
@@ -88,29 +88,17 @@ class DemoApplicationTests {
 
 		}
 
-		// uncomment this out and it works as expected
+		@Autowired
+		@Lazy
+		public void setDelegate(WebFilter delegate) {
+			this.delegate = delegate;
+		}
+
+		// uncomment this and the test passes
 		/*@Bean
-		public WebfluxWebFilter manual(Bucket4JBootProperties properties, AsyncCacheResolver asyncCacheResolver) {
-			final Bucket4JConfiguration conf = properties.getFilters().get(0);
-			FilterConfiguration filterConfig = new FilterConfiguration<>();
-			filterConfig.setUrl(conf.getUrl());
-			filterConfig.setOrder(conf.getFilterOrder());
-			filterConfig.setStrategy(conf.getStrategy());
-			filterConfig.setHttpResponseBody(conf.getHttpResponseBody());
-			filterConfig.setHttpResponseHeaders(conf.getHttpResponseHeaders());
-			filterConfig.setMetrics(conf.getMetrics());
-			filterConfig.getRateLimitChecks().add(new RateLimitCheck() {
-
-				@Override
-				public ConsumptionProbeHolder rateLimit(Object request, boolean async) {
-					// hard-coding this to make demo easier
-					return new ConsumptionProbeHolder(asyncCacheResolver.resolve("buckets").getProxy("/test-rate-limit").get().tryConsumeAndReturnRemaining(1));
-				}
-
-			});
-			return new WebfluxWebFilter(filterConfig);
+		public WebFilter custom() {
+			return (exchange, chain) -> delegate.filter(exchange, chain);
 		}*/
-
 	}
 
 }
